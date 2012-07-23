119c5cd7127e8f8313b480452ad27de24154c101
vr <vr@vr-Latitude-E6520.(none)>
2012-07-23 10:29:53 +0200

    add recursion to rm (rm -rf)

--- src/cmd_rm.c
+++ src/cmd_rm.c
@@ -20,11 +20,78 @@ int cmd_rm(int argc, char **argv);
 
 struct usage_def rm_usage[] =
   {
+    {'r', 0u, NULL, "recurse"},
+    {'f', 0u, NULL, "force"},
+    {'v', 0u, NULL, "verbose"},
     {USAGE_NO_OPT, USAGE_MANDAT, "path", "remote file"},
     {0, 0u, NULL, NULL},
   };
 
 struct cmd_def rm_cmd = {"rm", "remove file", rm_usage, cmd_rm};
+
+dpl_status_t
+recurse(dpl_ctx_t *ctx,
+        char *dir,
+        int verbose,
+        int level)
+{
+  void *dir_hdl;
+  dpl_dirent_t dirent;
+  int ret;
+
+  ret = dpl_chdir(ctx, dir);
+  if (DPL_SUCCESS != ret)
+    return ret;
+
+  ret = dpl_opendir(ctx, ".", &dir_hdl);
+  if (DPL_SUCCESS != ret)
+    return ret;
+
+  while (!dpl_eof(dir_hdl))
+    {
+      ret = dpl_readdir(dir_hdl, &dirent);
+      if (DPL_SUCCESS != ret)
+        return ret;
+
+      if (strcmp(dirent.name, "."))
+        {
+          int i;
+
+          if (DPL_FTYPE_DIR == dirent.type)
+            {
+              ret = recurse(ctx, dirent.name, verbose, level + 1);
+              if (DPL_SUCCESS != ret)
+                return ret;
+            }
+
+          if (verbose)
+            {
+              for (i = 0;i < level;i++)
+                printf(" ");
+              printf("%s\n", dirent.name);
+            }
+
+          if (DPL_FTYPE_DIR == dirent.type)
+            ret = dpl_rmdir(ctx, dirent.name);
+          else
+            ret = dpl_unlink(ctx, dirent.name);
+
+          if (DPL_SUCCESS != ret)
+            {
+              fprintf(stderr, "status: %s (%d)\n", dpl_status_str(ret), ret);
+              return ret;
+            }
+        }
+    }
+
+  dpl_closedir(dir_hdl);
+
+  ret = dpl_chdir(ctx, "..");
+  if (DPL_SUCCESS != ret)
+    return ret;
+
+  return DPL_SUCCESS;
+}
 
 int
 cmd_rm(int argc,
@@ -33,6 +100,9 @@ cmd_rm(int argc,
   int ret;
   char opt;
   char *path = NULL;
+  int rflag = 0;
+  int fflag = 0;
+  int verbose = 0;
 
   var_set("status", "1", VAR_CMD_SET, NULL);
 
@@ -41,6 +111,15 @@ cmd_rm(int argc,
   while ((opt = linux_getopt(argc, argv, usage_getoptstr(rm_usage))) != -1)
     switch (opt)
       {
+      case 'r':
+        rflag = 1;
+        break ;
+      case 'f':
+        fflag = 1;
+        break ;
+      case 'v':
+        verbose = 1;
+        break ;
       case '?':
       default:
         usage_help(&rm_cmd);
@@ -57,11 +136,31 @@ cmd_rm(int argc,
 
   path = argv[0];
 
-  ret = dpl_unlink(ctx, path);
-  if (DPL_SUCCESS != ret)
+  if (rflag)
     {
-      fprintf(stderr, "status: %s (%d)\n", dpl_status_str(ret), ret);
-      goto end;
+      ret = recurse(ctx, path, verbose, 0);
+      if (DPL_SUCCESS != ret)
+        {
+          fprintf(stderr, "error recursing: %s (%d)\n", dpl_status_str(ret), ret);
+          ret = 1;
+          goto end;
+        }
+
+      ret = dpl_rmdir(ctx, path);
+      if (DPL_SUCCESS != ret)
+        {
+          fprintf(stderr, "status: %s (%d)\n", dpl_status_str(ret), ret);
+          goto end;
+        }
+    }
+  else
+    {
+      ret = dpl_unlink(ctx, path);
+      if (DPL_SUCCESS != ret)
+        {
+          fprintf(stderr, "status: %s (%d)\n", dpl_status_str(ret), ret);
+          goto end;
+        }
     }
 
   var_set("status", "0", VAR_CMD_SET, NULL);
