e4887756adcc9031100e7e4f4ee531e37ffe4ce3
vr <vr@vr-Latitude-E6520.(none)>
2012-09-21 11:34:47 +0200

    cmd_cp is now only cp

--- src/cmd_cp.c
+++ src/cmd_cp.c
@@ -20,11 +20,6 @@ int cmd_cp(int argc, char **argv);
 
 struct usage_def cp_usage[] =
   {
-    {'c', 0u, NULL, "copy (default)"},
-    {'s', 0u, NULL, "symlink"},
-    {'l', 0u, NULL, "hardlink"},
-    {'m', 0u, NULL, "move"},
-    {'d', 0u, NULL, "mkdent"},
     {USAGE_NO_OPT, USAGE_MANDAT, "local_file", "local file"},
     {USAGE_NO_OPT, USAGE_MANDAT, "remote_file", "remote file"},
     {0, 0u, NULL, NULL},
@@ -40,7 +35,6 @@ cmd_cp(int argc,
   char opt;
   char *src_path = NULL;
   char *dst_path = NULL;
-  dpl_copy_directive_t copy_directive = DPL_COPY_DIRECTIVE_COPY;
 
   var_set("status", "1", VAR_CMD_SET, NULL);
 
@@ -49,21 +43,6 @@ cmd_cp(int argc,
   while ((opt = linux_getopt(argc, argv, usage_getoptstr(cp_usage))) != -1)
     switch (opt)
       {
-      case 'c':
-        copy_directive = DPL_COPY_DIRECTIVE_COPY;
-        break ;
-      case 's':
-        copy_directive = DPL_COPY_DIRECTIVE_SYMLINK;
-        break ;
-      case 'l':
-        copy_directive = DPL_COPY_DIRECTIVE_LINK;
-        break ;
-      case 'm':
-        copy_directive = DPL_COPY_DIRECTIVE_MOVE;
-        break ;
-      case 'd':
-        copy_directive = DPL_COPY_DIRECTIVE_MKDENT;
-        break ;
       case '?':
       default:
         usage_help(&cp_cmd);
@@ -97,7 +76,7 @@ cmd_cp(int argc,
         }
     }
 
-  ret = dpl_fcopy_ex(ctx, src_path, dst_path, copy_directive);
+  ret = dpl_fcopy(ctx, src_path, dst_path);
   if (DPL_SUCCESS != ret)
     {
       fprintf(stderr, "status: %s (%d)\n", dpl_status_str(ret), ret);
